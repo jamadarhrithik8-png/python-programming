409c211c41f6f92e5230bcdf6954037f79d57db5
Rithik <jamadarhrithik8@gmail.com>
2026-05-06 21:21:31 +0530

    Add files via upload

---
+++ python10.py
@@ -0,0 +1,17 @@
+L1=[1,2,3,4,5,6,7,8,9,10]
+L2=['Apple','Banana','Cherry','Date','Elderberry']
+L3=[True, False, True, False, True]
+print("max of L1 is", max(L1))
+print("max of L2 is", max(L2))
+print("max of L3 is", max(L3))
+print("min of L1 is", min(L1))
+print("min of L2 is", min(L2))
+print("min of L3 is", min(L3))
+print("sum of L1 is", sum(L1))
+print("sorted L1 is", sorted(L1))
+L1.append(11)
+print("L1 after appending 11 is", L1)
+L2.reverse()
+print("L2 after reversing is", L2)
+L3.count(True)
+print("Count of True in L3 is", L3.count(True))
