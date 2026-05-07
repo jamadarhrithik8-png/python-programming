c7b392d798590602ea6ea9915bdbf78889d727ab
Rithik <jamadarhrithik8@gmail.com>
2026-05-07 10:38:06 +0530

    Add files via upload

---
+++ python.12.py
@@ -0,0 +1,6 @@
+student={"name":"kiran","age":20,"course":"python"}
+print("name:", student["name"])
+print("age:", student["age"])
+print("course:", student["course"])
+student["age"]=21
+print("Updated age:", student["age"])
